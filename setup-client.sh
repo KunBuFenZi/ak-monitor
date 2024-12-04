ae911e576451fdd4a2b09941468407725f284d8f
KunBuFenZi <1462632457@qq.com>
2024-12-04 18:51:04 +0800

    Update setup-client.sh

--- setup-client.sh
+++ setup-client.sh
@@ -10,8 +10,21 @@ systemctl stop ak_client
 
 # Function to detect main network interface
 get_main_interface() {
+   # 检查是否安装了bc命令
+   if ! command -v bc > /dev/null; then
+       echo "Installing bc..."
+       if command -v apt-get > /dev/null; then
+           apt-get update && apt-get install -y bc
+       elif command -v yum > /dev/null; then
+           yum update -y && yum install -y bc
+       else
+           echo "Could not install bc. Please install it manually."
+           exit 1
+       fi
+   fi
+
    local interfaces=$(ip -o link show | \
-       awk -F': ' '$2 !~ /^(lo|docker|veth|br-|virbr|tun|bond|vnet|wg|vmbr|dummy|gre|sit|vlan|lxc|lxd|tap)/{print $2}' | \
+       awk -F': ' '$2 !~ /^(lo|docker|veth|br-|virbr|tun|vnet|wg|vmbr|dummy|gre|sit|vlan|lxc|lxd|tap)/{print $2}' | \
        grep -v '@')
    
    local interface_count=$(echo "$interfaces" | wc -l)
